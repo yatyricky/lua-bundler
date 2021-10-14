8ea44d5d83cd570d9b91f811a9e6015cca37cb41
Keos <turbogara1@gmail.com>
2021-10-14 18:42:24 +0900

    [Edit]log메세지 색추가

--- logger.js
+++ logger.js
@@ -1,10 +1,10 @@
 module.exports = {
     success: function (...params) {
-        console.log("✔", ...params)
+        console.log("\u001b[32m✔", ...params,"\u001b[0m")
     },
     log: console.log,
     warn: function (...params) {
-        console.log("⚠", ...params)
+        console.log("\u001b[31m⚠\u001b[0m", ...params)
     },
     error: function (...params) {
         console.log("❌", ...params)
