99290912003b31f64918f3a46ba96b90311f45f7
Rick Sun <yatyricky@gmail.com>
2026-04-22 23:35:26 +0800

    args

--- bin.js
+++ bin.js
@@ -3,7 +3,7 @@ import * as main from "./main.js"
 
 const args = process.argv.slice(2)
 
-const help = "lua-bundler <input.lua> <output.lua> [-p|--production] [-e|--exclude <path1;path2;...>] [-d|--define <FLAG1;FLAG2;...>]"
+const help = "lua-bundler -i <input.lua> -o <output.lua> [-p|--production] [-e|--exclude <path1;path2;...>] [-d|--define <FLAG1;FLAG2;...>]"
 
 let input = undefined
 let output = undefined
@@ -14,10 +14,26 @@ let defines = []
 for (let i = 0; i < args.length; i++) {
     if (args[i] === "-p" || args[i] === "--production") {
         production = true
+    } else if (args[i] === "-i" || args[i] === "--input") {
+        i++
+        if (args[i] === undefined) {
+            logger.error(`${args[i - 1]} must be followed by a file path`)
+            logger.log(help)
+            process.exit(1)
+        }
+        input = args[i]
+    } else if (args[i] === "-o" || args[i] === "--output") {
+        i++
+        if (args[i] === undefined) {
+            logger.error(`${args[i - 1]} must be followed by a file path`)
+            logger.log(help)
+            process.exit(1)
+        }
+        output = args[i]
     } else if (args[i] === "-e" || args[i] === "--exclude") {
         i++
         if (args[i] === undefined) {
-            logger.error(`-e must be followed with list paths`)
+            logger.error(`${args[i - 1]} must be followed with list of paths`)
             logger.log(help)
             process.exit(1)
         }
@@ -25,30 +41,20 @@ for (let i = 0; i < args.length; i++) {
     } else if (args[i] === "-d" || args[i] === "--define") {
         i++
         if (args[i] === undefined) {
-            logger.error(`-d must be followed with list of flags`)
+            logger.error(`${args[i - 1]} must be followed with list of flags`)
             logger.log(help)
             process.exit(1)
         }
         defines = args[i].split(";")
-    } else if (args[i].startsWith("-")) {
-        logger.error(`unknown option ${args[i]}.`)
+    } else {
+        logger.error(`unknown argument ${args[i]}.`)
         logger.log(help)
         process.exit(1)
-    } else {
-        if (input === undefined) {
-            input = args[i]
-        } else if (output === undefined) {
-            output = args[i]
-        } else {
-            logger.error(`unknown argument ${args[i]}.`)
-            logger.log(help)
-            process.exit(1)
-        }
     }
 }
 
 if (input === undefined || output === undefined) {
-    logger.error(`must specify input or output`)
+    logger.error(`-i and -o are required`)
     logger.log(help)
     process.exit(1)
 }
